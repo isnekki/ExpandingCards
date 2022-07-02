e9e80ee3b8db7737f064ec592330ad21fd577f50
Kane Taylor <kanetaylor02@gmail.com>
2022-07-02 21:02:14 +0800

    add resizing logic and image styling

---
+++ script.js
@@ -0,0 +1,25 @@
+const container = document.querySelector('.container')
+const NUMBER_OF_IMAGES = 5
+
+const onImageContainerClickHandler = (e) => {
+    const current = document.querySelector('.selected')
+    const clicked = e.currentTarget
+    current.classList.remove('selected')
+    clicked.classList.add('selected')
+}
+
+for (let i = 0; i < NUMBER_OF_IMAGES; i++) {
+    const imageContainer = document.createElement('div')
+    const image = document.createElement('img')
+
+    imageContainer.classList.add('imageContainer')
+    if (i === 0) imageContainer.classList.add('selected')
+
+    imageContainer.addEventListener('click', onImageContainerClickHandler)
+
+    image.src = `/assets/img${i + 1}.jpg`
+    image.alt = `Image ${i + 1}`
+    imageContainer.appendChild(image)
+    container.appendChild(imageContainer)
+}
+
